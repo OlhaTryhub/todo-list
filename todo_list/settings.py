c274afb546269d6bb401391bdae748e27dcb3fd5
Olha Tryhub <olha.tryhub@nure.ua>
2023-07-26 11:12:38 +0300

    Add models to app and migrations

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -87,8 +87,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-AUTH_USER_MODEL = "user.User"
-
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
