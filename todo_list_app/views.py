eabfd284414edb83552468d2654e890df62ffa32
Olha Tryhub <olha.tryhub@nure.ua>
2023-07-26 13:19:45 +0300

    Add main page: url, view, template, base.html, includes

--- todo_list_app/views.py
+++ todo_list_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+
+from todo_list_app.models import Task
+
 
 # Create your views here.
+class TaskListView(generic.ListView):
+    model = Task
+    queryset = Task.objects.prefetch_related("tags")
+    template_name = "task/task_list.html"
+
